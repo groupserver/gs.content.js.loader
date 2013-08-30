c3a5d3d7e2e0247caef6985bccf1397e2f0ae17d
Michael JasonSmith <mpj17@onlinegroups.net>
2013-08-30 14:42:41 -0700

    Adding a copyright and license.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
 import os
 from setuptools import setup, find_packages
 from version import get_version
@@ -11,21 +11,21 @@ setup(name='gs.content.js.loader',
     long_description=open("README.txt").read() + "\n" +
                       open(os.path.join("docs", "HISTORY.txt")).read(),
     classifiers=[
-      "Development Status :: 4 - Beta",
-      "Environment :: Web Environment",
-      "Framework :: Zope2",
-      "Intended Audience :: Developers",
-      "License :: Other/Proprietary License",
-      "Natural Language :: English",
-      "Operating System :: POSIX :: Linux"
-      "Programming Language :: JavaScript",
-      "Topic :: Software Development :: Libraries :: JavaScript Modules",
+        'Development Status :: 5 - Production/Stable',
+        "Environment :: Web Environment",
+        "Framework :: Zope2",
+        "Intended Audience :: Developers",
+        'License :: OSI Approved :: Zope Public License',
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux"
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
       ],
     keywords='javascript Loader async',
     author='Michael JasonSmith',
     author_email='mpj17@onlinegroups.net',
     url='http://groupserver.org/',
-    license='GPL',
+    license='ZPL 2.0',
     packages=find_packages(exclude=['ez_setup']),
     namespace_packages=['gs','gs.content','gs.content.js'],
     include_package_data=True,
@@ -33,6 +33,7 @@ setup(name='gs.content.js.loader',
     install_requires=[
         'setuptools',
     ],
+    extras_require={'zope': ['zope.browserresource', ]},
     entry_points="""
     # -*- Entry points: -*-
     """,
